6666557d8d74ba99de246a66c6c0fb1189109eee
Ognian Genev <team@jolyy.com>
2018-06-07 20:08:47 +0300

    merge to master

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import '../styles/App.css'
+import Header from './Header'
+import Main from './Main'
 
 class App extends Component {
   render () {
     return (
-      <div className='App' />
+      <Fragment>
+        <Header />
+        <Main />
+      </Fragment>
     )
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  flex: {
+    flex: 1
+  }
+}
+
+function Header (props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='title' color='inherit' align='center' className={classes.flex}>
+            Demo Chat
+          </Typography>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Header)
