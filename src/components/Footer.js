98436f27b36b1555a71518e242dbea539f731e76
Ognian Genev <team@jolyy.com>
2018-06-09 20:58:14 +0300

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
+import HomeIcon from '@material-ui/icons/Home';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
@@ -35,9 +35,9 @@ class Footer extends React.Component {
         className={classes.root}
         style={{position: 'absolute',bottom: 0, right: 0}}
       >
-        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Начало" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Любими" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="До мен" icon={<LocationOnIcon />} />
       </BottomNavigation>
     );
   }
