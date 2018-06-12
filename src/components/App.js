823a5fe540a7856f231077bcfa5df7cc49d3b30d
Ognian Genev <team@jolyy.com>
2018-06-12 22:40:33 +0300

    App authUser state added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component, Fragment } from 'react'
 import ResDrawer from './ResDrawer'
+import firebase from 'firebase/app'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      authUser: null
+    }
+  }
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ authUser: 1 })
+      } else {
+        this.setState({ authUser: null })
+      }
+    })
+  }
+
   render () {
     return (
       <Fragment>
-        <ResDrawer />
+        <ResDrawer authUser={this.state.authUser} />
       </Fragment>
     )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,16 @@
+import firebase from 'firebase/app'
 import React from 'react'
-import PropTypes from 'prop-types'
-import Button from '@material-ui/core/Button'
-import { withStyles } from '@material-ui/core/styles'
 
-const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit
-  },
-  input: {
-    display: 'none'
-  }
-})
+const Home = () =>
+  <button
+    type='button'
+    onClick={() => (firebase.auth().signOut().then(function () {
+      console.log('sign in success')
+    }).catch(function (error) {
+      console.log(error)
+    }))}
+  >
+    Sign Out
+  </button>
 
-function Home (props) {
-  const { classes } = props
-  return (
-    <div>
-      <Button variant='outlined' className={classes.button}
-        onClick={() => (console.log('click'))}>
-        Sign out
-      </Button>
-    </div>
-  )
-}
-
-Home.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(Home)
+export default Home
