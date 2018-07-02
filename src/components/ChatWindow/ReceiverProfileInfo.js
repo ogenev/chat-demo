da063f22f226cdfc4cf5e469c57e4348d51f2419
Ognian Genev <team@jolyy.com>
2018-07-02 12:50:03 +0300

    added css

--- src/components/ChatWindow/ReceiverProfileInfo.js
+++ src/components/ChatWindow/ReceiverProfileInfo.js
@@ -4,17 +4,54 @@ import { withStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 
 const styles = () => ({
+  online: {
+    display: 'inline-block',
+    color: '#e6e6e6',
+    fontSize: '0.85rem',
+    fontWeight: 300
+  },
+  icon: {
+    display: 'inline-block',
+    paddingLeft: 2
+  },
+  onlineContainer: {
+    textAlign: 'center',
+    position: 'relative',
+    bottom: '0.4em'
+  },
+  username: {
+    fontSize: '1.3rem',
+    position: 'relative',
+    bottom: 6
+  },
+  circle: {
+    background: '#34E614',
+    display: 'inline-block',
+    height: 8,
+    width: 8,
+    borderRadius: 4
+  }
 
 })
 
 class ReceiverProfileInfo extends React.Component {
   render () {
-    // const { classes } = this.props
+    const { classes } = this.props
 
     return (
-      <Typography color={'inherit'}>
-        Receiver username
-      </Typography>
+      <div>
+        <Typography color={'inherit'} variant={'subheading'} align={'center'}
+          className={classes.username}>
+        palqkat
+        </Typography>
+        <div className={classes.onlineContainer}>
+          <Typography className={classes.online} >
+          online 3w ago
+          </Typography>
+          <div className={classes.icon} />
+          <span className={classes.circle} />
+        </div>
+      </div>
     )
   }
 }
